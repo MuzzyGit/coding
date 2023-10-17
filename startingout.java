1ebd4feee86ffa32912b006cd1f71251cc15b773
Maxim Tsed <maxtsed@gmail.com>
2023-10-16 22:54:05 -0500

    second commit?

--- startingout.java
+++ startingout.java
@@ -1,6 +1,7 @@
 public class startingout {
     public static void main(String[] args) {
         System.out.println("Hello World! it has been quite a long time!");
+        System.out.println("We are about to make a second commit hopefully");
     }
 }
 
