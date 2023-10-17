4ab600c856142d22d34975a5a17bb94dba4d0746
Maxim Tsed <maxtsed@gmail.com>
2023-10-16 22:44:30 -0500

    first commit

---
+++ startingout.java
@@ -0,0 +1,50 @@
+public class startingout {
+    public static void main(String[] args) {
+        System.out.println("Hello World! it has been quite a long time!");
+    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
